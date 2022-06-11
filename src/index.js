9d319cc69b76873a4837049794724abe5bd592bd
Cruzstyle <106716525+Cruzstyle@users.noreply.github.com>
2022-06-11 19:12:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,9 @@ const Application = () => {
       <MoralisProvider  appId={APP_ID}
       serverUrl={SERVER_URL}>
         <MoralisDappProvider>
+        <MediaQuery minWidth={250}>
            <App/>
+          </MediaQuery>
         </MoralisDappProvider>
       </MoralisProvider>
     );
